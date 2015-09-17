e990788ba407ccf75ddb1612d3ebd3b10e0ae558
steveMQ <stevequatrale@gmail.com>
2015-09-17 16:53:40 -0400

    Cleaned up the code, :simple_smile:

---
+++ inc/portfolio-navigation.php
@@ -0,0 +1,27 @@
+<?php
+
+  function getPreviousPortfolioLink() {
+    if( get_adjacent_post(false, '', true) ) {
+      previous_post_link('%link', getPostLinkText('left'));
+    } else {
+        $args = array('post_type' => 'portfolio', 'posts_per_page' => '1', 'order' => 'DESC');
+        $first = new WP_Query($args);
+        $first->the_post();
+          echo '<a href="' . get_permalink() . '">' . getPostLinkText('left') . '</a>';
+        wp_reset_query();
+    };
+  }
+
+  function getNextPortfolioLink() {
+    if( get_adjacent_post(false, '', false) ) {
+      next_post_link('%link', getPostLinkText('right'));
+    } else {
+        $args = array('post_type' => 'portfolio', 'posts_per_page' => 1, 'order' => 'ASC');
+        $last = new WP_Query($args);
+        $last->the_post();
+          echo '<a href="' . get_permalink() . '">' . getPostLinkText('right') . '</a>';
+        wp_reset_query();
+    };
+  }
+
+?>
--- single-portfolio.php
+++ single-portfolio.php
@@ -27,30 +27,10 @@
 			<div class="row">
 				<div class="single-portfolio-arrows">
 					<div class="left-arrow">
-						<?php
-							if( get_adjacent_post(false, '', true) ) {
-								previous_post_link('%link', getPostLinkText('left'));
-							} else {
-									$args = array('post_type' => 'portfolio', 'posts_per_page' => '1', 'order' => 'DESC');
-							    $first = new WP_Query($args);
-									$first->the_post();
-							    	echo '<a href="' . get_permalink() . '">' . getPostLinkText('left') . '</a>';
-							  	wp_reset_query();
-							};
-						?>
+						<?php getPreviousPortfolioLink(); ?>
 					</div>
 					<div class="right-arrow">
-						<?php
-							if( get_adjacent_post(false, '', false) ) {
-								next_post_link('%link', getPostLinkText('right'));
-							} else {
-									$args = array('post_type' => 'portfolio', 'posts_per_page' => 1, 'order' => 'ASC');
-							    $last = new WP_Query($args);
-									$last->the_post();
-							    	echo '<a href="' . get_permalink() . '">' . getPostLinkText('right') . '</a>';
-							  	wp_reset_query();
-							};
-						?>
+						<?php getNextPortfolioLink(); ?>
 					</div>
 
 				</div>
