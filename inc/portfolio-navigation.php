c717d0c22977142c4e7b5b7df52d9dcabd92f01e
steveMQ <stevequatrale@gmail.com>
2015-09-17 17:12:45 -0400

    :lipstick: round 2

--- inc/portfolio-navigation.php
+++ inc/portfolio-navigation.php
@@ -5,25 +5,25 @@
   }
 
   function getPreviousPortfolioLink() {
-    if( get_adjacent_post(false, '', true) ) {
+    if( get_previous_post(false, '') ) {
       previous_post_link('%link', getPostLinkText('left'));
     } else {
         $args = array('post_type' => 'portfolio', 'posts_per_page' => '1', 'order' => 'DESC');
         $first = new WP_Query($args);
         $first->the_post();
-          echo '<a href="' . get_permalink() . '">' . getPostLinkText('left') . '</a>';
+        echo '<a href="' . get_permalink() . '">' . getPostLinkText('left') . '</a>';
         wp_reset_query();
     };
   }
 
   function getNextPortfolioLink() {
-    if( get_adjacent_post(false, '', false) ) {
+    if( get_next_post(false, '') ) {
       next_post_link('%link', getPostLinkText('right'));
     } else {
         $args = array('post_type' => 'portfolio', 'posts_per_page' => 1, 'order' => 'ASC');
         $last = new WP_Query($args);
         $last->the_post();
-          echo '<a href="' . get_permalink() . '">' . getPostLinkText('right') . '</a>';
+        echo '<a href="' . get_permalink() . '">' . getPostLinkText('right') . '</a>';
         wp_reset_query();
     };
   }
